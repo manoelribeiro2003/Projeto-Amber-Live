5f708eb3d09ce28a8baa80b41745a5fbffc4f046
Renato Walbert Bastos de Menezes <rwallbert@hotmail.com>
2023-12-13 09:49:18 -0300

    based

--- home/php/conexao.php
+++ home/php/conexao.php
@@ -1,6 +1,6 @@
 <?php
 include_once("cores.php");
-$conn = new Mysqli('localhost','root','','amber_live');
+$conn = new Mysqli('localhost','root','','amberlive');
 
 if ($conn->connect_error) {
     echo('Erro ao conectar-se ao banco de dados'.$conn->connect_error);
