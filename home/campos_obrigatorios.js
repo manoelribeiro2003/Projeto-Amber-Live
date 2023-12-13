089c5ab878704ba6f5930f5fb5414618e9060e97
Lohan1 <italolohan244@gmail.com>
2023-12-13 09:46:24 -0300

    dia 13/12

--- home/campos_obrigatorios.js
+++ home/campos_obrigatorios.js
@@ -1,4 +1,4 @@
-function V_produto(campo) {
+function V_email(campo) {
     if (campo.value.trim() == '') {
         p = document.getElementById("alertaEmail").innerHTML = '*Campo obrigatório';
         document.getElementById("alertaEmail").style.color = 'red';
@@ -16,54 +16,9 @@ function V_senha(campo) {
     }
 }
 
-function V_valor(campo) {
-    if (campo.value.trim() == '') {
-        p = document.getElementById("alertaSenha").innerHTML = '*Campo obrigatório';
-        document.getElementById("alertaSenha").style.color = 'red';
-    } else if (campo.value < 0) {
-        document.getElementById("alertaSenha").innerHTML = '* O valor não pode ser negativo';
-        document.getElementById("alertaSenha").style.color = 'red';
-        campo.value = "";
-        campo.focus();
-    } else {
-        document.getElementById("alertaSenha").innerHTML = '';
-    }
-}
-
-function V_quantidade(campo) {
-    if (campo.value.trim() == '') {
-        p = document.getElementById("alertaQuantidade").innerHTML = '*Campo obrigatório';
-        document.getElementById("alertaQuantidade").style.color = 'red';
-    }
-    else if (campo.value < 0) {
-        document.getElementById("alertaQuantidade").innerHTML = '* A quantidade não pode ser negativa';
-        document.getElementById("alertaQuantidade").style.color = 'red';
-        campo.value = "";
-        campo.focus();
-    } else {
-        document.getElementById("alertaQuantidade").innerHTML = '';
-    }
-}
-
-
-
 function V_cadastrar() {
-    p = document.getElementById("produto");
-    v = document.getElementById("valor");
-    q = document.getElementById("quantidade");
-    d = document.getElementById("validade");
-    V_produto(p);
+    p = document.getElementById("alertaEmail");
+    v = document.getElementById("alertaSenha");
+    V_email(p);
     V_valor(v);
-    V_quantidade(q);
-    V_validade(d);
 }
-
-function V_pesquisa(campo) {
-    if (campo.value.trim() == '') {
-        p = document.getElementById("alertaPesquisa").innerHTML = '*Campo obrigatório';
-        document.getElementById("alertaPesquisa").style.color = 'red';
-    } else {
-        document.getElementById("alertaPesquisa").innerHTML = '';
-    }
-}
-
